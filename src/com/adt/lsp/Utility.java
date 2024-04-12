fa10439df1c3fe9c219a2f17f965e48a05307e24
sankardurai17 <sankaranbu2000@gmail.com>
2024-04-12 12:23:39 -0400

    update comments

--- src/com/adt/lsp/Utility.java
+++ src/com/adt/lsp/Utility.java
@@ -1,12 +1,13 @@
 package com.adt.lsp;
 
 public class Utility {
-//TODO
+//TODO we can have different methods that would deal with different cases
+    //TODO one method for Converting online File to Graph
     public static void onlineFileToGraphConverter(String fileName){
 
 
     }
-
+    //TODO one method for Converting random File with edges along with its cocordinates to Graph
     public static void randomFileToGraphConverter(String fileName){
 
 
