8d7780f1aa4c9a7f5956a30027b6375cdf2d6d4e
sankardurai17 <sankaranbu2000@gmail.com>
2024-04-12 12:20:32 -0400

    Online Graphs

---
+++ src/com/adt/lsp/Utility.java
@@ -0,0 +1,18 @@
+package com.adt.lsp;
+
+public class Utility {
+//TODO
+    public static void onlineFileToGraphConverter(String fileName){
+
+
+    }
+
+    public static void randomFileToGraphConverter(String fileName){
+
+
+    }
+
+    public static void converter(){
+
+    }
+}
