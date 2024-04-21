2130e8c6152ef8b4fdc57e9d4563f49311e92055
sankardurai17 <sankaranbu2000@gmail.com>
2024-04-21 14:58:34 -0400

    Dijkstra updates

--- src/com/adt/lsp/Dijkstra.java
+++ src/com/adt/lsp/Dijkstra.java
@@ -8,81 +8,25 @@ import java.util.*;
 
 public class Dijkstra {
 
-    public static int findLSPUsingDijkstra(GeometricGraph graph,List<Vertex> lcc){
+    public static int findLSPUsingDijkstra(GeometricGraph graph){
+        List<Vertex> lcc = LongestConnectedComponent.getLargestConnectedComponent(graph);
         int max_depth=Integer.MIN_VALUE;
         Map<Integer,Integer> maxList=new HashMap<>();
-        int mainSourceOfLSP=-1;
-        Vertex sourceVertex;
+
         for(Vertex vertex:lcc){
-
-                //System.out.println("Vertex Source: " + vertex);
-                int depth = dijkstraMAX(graph, lcc, vertex);
-                maxList.put(vertex.id, depth);
-                if(depth>max_depth) {
-                    max_depth=depth;
-                    mainSourceOfLSP= vertex.id;;
-                }
-
-                /*int depth=dijkstraMAX(graph, lcc, vertex);
-                maxList.put(vertex.id,depth);
-                max_depth=Integer.max(depth,max_depth);*/
+            int depth = dijkstraMAX(graph, lcc, vertex);
+            maxList.put(vertex.id, depth);
+            if(depth>max_depth) {
+                max_depth=depth;
+            }
         }
-        System.out.println("--------Max_Depth Dijkstra: "+max_depth);
-        System.out.println("--------Source ID:" +mainSourceOfLSP );
         return max_depth;
     }
 
-
-
-    private static void relaxMAX(Vertex u, Vertex v, Map<Vertex, Integer> distance, Map<Vertex, Vertex> predecessor) {
-        if (distance.get(v) < distance.get(u) + 1) {
-            distance.put(v, distance.get(u) + 1);
-            predecessor.put(v, u);
-        }
-    }
-
-
-    public static int dijkstraMAXOG(GeometricGraph graph, List<Vertex> LCC, Vertex sourceVertex) {
-        List<Vertex> tempLcc=new ArrayList<>(LCC);
+    public static int dijkstraMAX(GeometricGraph graph, List<Vertex> LCC, Vertex sourceVertex) {
         Map<Vertex, List<Vertex>> adjacencyList = graph.getAdjacencyList();
-        int[] d = new int[graph.getN()+1]; // Distance array
-        Arrays.fill(d, Integer.MIN_VALUE); // Initialize with -∞
-        int source= sourceVertex.id;
-        d[source] = 0;
-
-        // Modified Dijkstra's algorithm
-        PriorityQueue<Vertex> Q = new PriorityQueue<>(Collections.reverseOrder());
-        Q.add(sourceVertex);
-
-
-        while (!Q.isEmpty()) {
-            Vertex u = Q.poll();
-            Set<Integer> visited=new HashSet<>();
-            //System.out.println("Vertex u: "+u);
-            visited.add(u.id);
-            List<Vertex> neighbors=adjacencyList.getOrDefault(u, Collections.emptyList());
-            //System.out.println("Neighbors:  "+neighbors);
-            for (Vertex v : neighbors) {
-                //change this
-                if (!visited.contains(v.id)&&d[v.id] <= d[u.id] + 1) {
-                    d[v.id] = d[u.id] + 1;
-                   // Q.remove(v);
-                    Q.add(v);
-                }
-            }
-        }
-
-        //System.out.println("Visited====="+visited);
-
-        // Finding the maximum distance
-        int maxDistance = getMaxDistance(graph, source, d);
-        return maxDistance;
-    }
-
-   public static int dijkstraMAX(GeometricGraph graph, List<Vertex> LCC, Vertex sourceVertex) {
-        Map<Vertex, List<Vertex>> adjacencyList = graph.getAdjacencyList();
-        int[] d = new int[graph.getN()+1]; // Distance array
-        Arrays.fill(d, Integer.MIN_VALUE); // Initialize with -∞
+        int[] d = new int[graph.getN()+1];
+        Arrays.fill(d, Integer.MIN_VALUE);
         int source= sourceVertex.id;
         d[source] = 0;
 
@@ -96,14 +40,11 @@ public class Dijkstra {
 
         while (!Q.isEmpty()) {
             Vertex u = Q.poll();
-            //System.out.println("Vertex u: "+u);
             visited.add(u.id);
             List<Vertex> neighbors=adjacencyList.getOrDefault(u, Collections.emptyList());
-            //Q.addAll(neighbors);
-            //System.out.println("Neighbors:  "+neighbors);
             for (Vertex v : neighbors) {
 
-                //change this
+                //relax max
                 if (!visited.contains(v.id) &&d[v.id] < d[u.id] + 1) {
                     d[v.id] = d[u.id] + 1;
                     Q.remove(v);
@@ -112,9 +53,6 @@ public class Dijkstra {
 
             }
         }
-
-        //System.out.println("Visited====="+visited);
-
         // Finding the maximum distance
         int maxDistance = getMaxDistance(graph, source, d);
         return maxDistance;
