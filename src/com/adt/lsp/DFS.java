6d58a107526bd3474d6eccecc8b0231a0fe105e3
sankardurai17 <sankaranbu2000@gmail.com>
2024-04-21 14:19:28 -0400

    DFS update

--- src/com/adt/lsp/DFS.java
+++ src/com/adt/lsp/DFS.java
@@ -9,16 +9,16 @@ import java.util.*;
 import static java.lang.Math.max;
 
 public class DFS {
-
-    private static void dfs(GeometricGraph graph, Vertex vertex, Set<Vertex> visited, int depth, Map<Vertex, Integer> depthMap) {
+    private static void depthFirstSearch(GeometricGraph graph, Vertex vertex, Set<Vertex> visited, int depth, Map<Vertex, Integer> depthMap) {
         visited.add(vertex);
         depthMap.put(vertex, depth);
 
         for (Vertex neighbor : graph.getAdjacencyList().getOrDefault(vertex, Collections.emptyList())) {
             if (!visited.contains(neighbor)) {
-                dfs(graph, neighbor, visited, depth + 1, depthMap);
+                depthFirstSearch(graph, neighbor, visited, depth + 1, depthMap);
             }
         }
+
     }
 
 
@@ -34,48 +34,34 @@ public class DFS {
         return vertexMaxDepth;
     }
 
-    public static int LongestSimplePath(GeometricGraph graph,List<Vertex> lcc) {
+    public static int LongestSimplePath(GeometricGraph graph) {
         int lmax = 0;
 
-        List<Vertex> vlccList = lcc;
-        System.out.println("VCCList:" + vlccList.size());
+        List<Vertex> vlccList = LongestConnectedComponent.getLargestConnectedComponent(graph);
         int vlccCount = vlccList.size();
-        Vertex source=null;
 
         for(int i=0; i< (int)Math.sqrt(vlccCount); i++){
-        //for (int i = 0; i < vlccCount; i++) {
             int randomIndex = new Random().nextInt(vlccCount);
-            //int randomIndex = i;
-            //System.out.println("Random Index:  " + randomIndex);
             Vertex randomVertex = vlccList.get(randomIndex);
-            //System.out.println("LCC Vertex: " + randomVertex);
 
             Map<Vertex, Integer> depthMap = new HashMap<>();
             Set<Vertex> visited = new HashSet<>();
-            dfs(graph, randomVertex, visited, 0, depthMap);
+            depthFirstSearch(graph, randomVertex, visited, 0, depthMap);
 
             Vertex vertexMaxDepth = findVertexAtMaxDepth(depthMap);
             int vDepth = depthMap.get(vertexMaxDepth);
-            //System.out.println("VertexMaxDepth: " + vertexMaxDepth + "\t Its size is:" + vDepth);
-            //System.out.println("Depth Map: " + depthMap);
-
-            //apply dfs on vertexMaxDepth to find max. depth vertex and wDepth
             depthMap.clear();
             visited.clear();
-            //System.out.println("DepthMap: " + depthMap);
-            dfs(graph, vertexMaxDepth, visited, 0, depthMap);
+
+            depthFirstSearch(graph, vertexMaxDepth, visited, 0, depthMap);
             Vertex vertexMaxDepth2 = findVertexAtMaxDepth(depthMap);
             int wDepth = depthMap.get(vertexMaxDepth2);
-            //System.out.println("VertexMaxDepth2: " + vertexMaxDepth2 + "\t Its size is:" + wDepth);
 
 
             lmax = max(lmax, max(vDepth, wDepth));
-            //System.out.println("\niteration:" + i + ", lmax:" + lmax);
         }
         return lmax;
     }
-
-
 }
 
 
